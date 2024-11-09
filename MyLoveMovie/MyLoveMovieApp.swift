7d6f209b9b75d798111193f29c0f93b34c8ba708
HaiPH <haiph0208@gmail.com>
2024-11-09 20:01:47 +0700

    Initial Commit

---
+++ MyLoveMovie/MyLoveMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoveMovieApp.swift
+//  MyLoveMovie
+//
+//  Created by HaiPH on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyLoveMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
